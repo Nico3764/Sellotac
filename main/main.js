c118c39665a50dfdd83d77c4e596fc4fd3bbf3a4
Nicolás Pablo Robles <nicolasroble@gmail.com>
2023-02-25 03:00:01 -0300

    Se programa corrousel + fonts

--- main/main.js
+++ main/main.js
@@ -4,14 +4,37 @@ let menuView=() =>
     document.getElementById('dropdownMenu').style.display = "block";
     
 }
+let corrouselActualiceView=() =>
+{
+    elementSelect= document.getElementById('imgCarrousel');
+    currentImgName=elementSelect.getAttribute("src");    
+    //console.log(elementSelect.getAttribute("src"));
+    for (let i = 1; i < 10; i++) {
+        imgName="./img/Carrousel" + i + ".jpeg";
+        //console.log("estoy comparando " + currentImgName + " con "+ imgName)
+        if (currentImgName === imgName){
+            imgName="./img/Carrousel" + (i+1) + ".jpeg";
+            if(imgName === "./img/Carrousel10.jpeg"){
+                imgName = "./img/Carrousel1.jpeg";
+                break;
+            }
+            //console.log("entra en el if "+i);
+            //console.log(" currentImgName es: "+typeof(imgName)+ " currentImgName es: "+ typeof(currentImgName)); 
+            break;
+        };
+    };  
+    
+    elementSelect.setAttribute("src", imgName );
+}
 let startview=() =>
 {
+    myInterval = setInterval(corrouselActualiceView, 4000);
+    document.getElementById('actMenu').addEventListener('mouseover', menuView);
 
-    document.getElementById('actMenu').addEventListener('click', menuView);
 }
 let start =() =>
 {
 	startview();
-    console.log("llega hasta aca...")
+    console.log("llega hasta aca...");
 }
 window.addEventListener('DOMContentLoaded', start );
