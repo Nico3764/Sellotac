ba7867df08acb11a5706cad59b7db8a606374f81
Nicolás Pablo Robles <nicolasroble@gmail.com>
2023-02-26 21:48:19 -0300

    Se generaliza funcion carrusel y se programa menu

--- main/main.js
+++ main/main.js
@@ -1,21 +1,32 @@
-let menuView=() =>
+let menuView=(action) =>
 {
-    document.getElementById('actMenu').style.display = "none";
-    document.getElementById('dropdownMenu').style.display = "block";
+    if(action === 'slide'){
+        document.getElementById('actMenu').style.display = "none";
+        document.getElementById('dropdownMenu').style.display = "block";    
+    };
+    myTimeout = setTimeout(()=>{
+        if (action === 'hidden'){
+            document.getElementById('actMenu').style.display = "block";
+            document.getElementById('dropdownMenu').style.display = "none";   
+        };
+    }, 100);
     
+    console.log(action);
 }
-let corrouselActualiceView=() =>
+
+let corrouselActualiceView=(route,id) =>
 {
-    elementSelect= document.getElementById('imgCarrousel');
+    elementSelect= document.getElementById(id);
     currentImgName=elementSelect.getAttribute("src");    
+    //console.log("route: "+route+"--->id: "+id);
     //console.log(elementSelect.getAttribute("src"));
     for (let i = 1; i < 10; i++) {
-        imgName="./img/Carrousel" + i + ".jpeg";
+        imgName=route+"Carrousel" + i + ".jpeg";
         //console.log("estoy comparando " + currentImgName + " con "+ imgName)
         if (currentImgName === imgName){
-            imgName="./img/Carrousel" + (i+1) + ".jpeg";
-            if(imgName === "./img/Carrousel10.jpeg"){
-                imgName = "./img/Carrousel1.jpeg";
+            imgName=route+"Carrousel" + (i+1) + ".jpeg";
+            if(imgName === route+"Carrousel10.jpeg"){
+                imgName = route+"Carrousel1.jpeg";
                 break;
             }
             //console.log("entra en el if "+i);
@@ -26,12 +37,14 @@ let corrouselActualiceView=() =>
     
     elementSelect.setAttribute("src", imgName );
 }
+
 let startview=() =>
 {
-    myInterval = setInterval(corrouselActualiceView, 4000);
-    document.getElementById('actMenu').addEventListener('mouseover', menuView);
-
+    myInterval = setInterval(()=>{corrouselActualiceView("./img/CarrouselPortada/", "imgCarrousel")}, 4000);
+    document.getElementById('actMenu').addEventListener('mouseover', ()=>{menuView('slide')});
+    document.getElementById('body').addEventListener('click', ()=>{menuView('hidden')});
 }
+
 let start =() =>
 {
 	startview();
